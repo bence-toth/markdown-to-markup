77c874dc29455c3453124ddc428f21ebc99cba06
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-06-08 13:38:15 +0200

    Implemented syntax highlighting with Prism.js

--- renderer/renderers/codeBlock.js
+++ renderer/renderers/codeBlock.js
@@ -1,7 +1,24 @@
+const prism = require('prismjs')
+
+const addSyntaxHighlight = ({code, language}) => {
+  const {[language]: languagePack} = Prism.languages
+  if (languagePack) {
+    const html = prism.highlight(code, languagePack, language)
+    return html
+  }
+  return code
+}
+
 const renderCodeBlock = ({
-  token: {lang, value}
+  token: {lang: language, value: code}
 }) => {
-  return `<code><pre>${value}</pre></code>`
+  const className = `language-${language}`
+  const classAttribute = `class="${className}"`
+  const content = addSyntaxHighlight({
+    code,
+    language
+  })
+  return `<pre ${classAttribute}><code ${classAttribute}>${content}</code></pre>`
 }
 
 module.exports = {renderCodeBlock}
