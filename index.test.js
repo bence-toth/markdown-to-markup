0db5e766c897f46716d10c2d23e285388ba645ff
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-06-19 23:35:21 +0200

    Restructured tests a bit

--- index.test.js
+++ index.test.js
@@ -1,36 +1,39 @@
 const {markdownToMarkup} = require('./index')
 const {JSDOM} = require('jsdom')
 
-test('Renders simple headings correctly', () => {
-  const text = 'Heading'
-  const headings = [
-    `# ${text}`,
-    `## ${text}`,
-    `### ${text}`,
-    `#### ${text}`,
-    `##### ${text}`,
-    `###### ${text}`
-  ]
-  headings.map((heading, headingIndex) => {
-    const dom = new JSDOM(`<!DOCTYPE html><div id="root">${markdownToMarkup(heading)}</div>`)
-    const root = dom.window.document.getElementById('root')
-    expect(root.children.length).toBe(1)
-    expect(root.children[0].tagName).toBe(`H${headingIndex + 1}`)
-    expect(root.children[0].textContent).toBe(text)
+describe('Headings', () => {
+  it('with # syntax should render correctly', () => {
+    const text = 'Heading'
+    const headings = [
+      `# ${text}`,
+      `## ${text}`,
+      `### ${text}`,
+      `#### ${text}`,
+      `##### ${text}`,
+      `###### ${text}`
+    ]
+    headings.map((heading, headingIndex) => {
+      const dom = new JSDOM(`<!DOCTYPE html><div id="root">${markdownToMarkup(heading)}</div>`)
+      const root = dom.window.document.getElementById('root')
+      expect(root.children.length).toBe(1)
+      expect(root.children[0].tagName).toBe(`H${headingIndex + 1}`)
+      expect(root.children[0].textContent).toBe(text)
+    })
+  })
+
+  it('with =/- syntax should render correctly', () => {
+    const text = 'Heading'
+    const headings = [
+      `${text}\n=======`,
+      `${text}\n-------`,
+    ]
+    headings.map((heading, headingIndex) => {
+      const dom = new JSDOM(`<!DOCTYPE html><div id="root">${markdownToMarkup(heading)}</div>`)
+      const root = dom.window.document.getElementById('root')
+      expect(root.children.length).toBe(1)
+      expect(root.children[0].tagName).toBe(`H${headingIndex + 1}`)
+      expect(root.children[0].textContent).toBe(text)
+    })
   })
 })
 
-test('Renders simple underscore headings correctly', () => {
-  const text = 'Heading'
-  const headings = [
-    `${text}\n=======`,
-    `${text}\n-------`,
-  ]
-  headings.map((heading, headingIndex) => {
-    const dom = new JSDOM(`<!DOCTYPE html><div id="root">${markdownToMarkup(heading)}</div>`)
-    const root = dom.window.document.getElementById('root')
-    expect(root.children.length).toBe(1)
-    expect(root.children[0].tagName).toBe(`H${headingIndex + 1}`)
-    expect(root.children[0].textContent).toBe(text)
-  })
-})
