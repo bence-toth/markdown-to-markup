729ad23f14f722df88142dbe6e7429d525a1620e
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-06-22 02:36:50 +0200

    Organised to-do items better

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 const {markdownToMarkup} = require('./index')
 const {JSDOM} = require('jsdom')
 
+// TODO: Blockquote with embedded markdown
 describe('Blockquote', () => {
   it('should render correctly', () => {
     const textContent = 'This is a blockquote.'
@@ -21,6 +22,8 @@ describe('Blockquote', () => {
     expect(paragraphNode.textContent).toBe(textContent)
   })
 })
+
+// TODO: Code block (syntax highlighting, copying to clipboard)
 
 describe('Emphasis', () => {
   it('should render correctly with every syntax', () => {
@@ -152,6 +155,33 @@ describe('HTML code', () => {
   })
 })
 
+// TODO: Image with and without title
+// TODO: Reference resolver - Images with and without title
+
+describe('Inline code', () => {
+  it('should render correctly', () => {
+    const textContent = 'run()'
+    const markdown = `You can just call \`${textContent}\` to start.`
+    const dom = new JSDOM(
+      `<!DOCTYPE html>
+      <div id="root">
+        ${markdownToMarkup(markdown)}
+      </div>`
+    )
+    const root = dom.window.document.getElementById('root')
+    expect(root.children.length).toBe(1)
+    const paragraphNode = root.children[0]
+    expect(paragraphNode.children.length).toBe(1)
+    const codeNode = root.children[0].children[0]
+    expect(codeNode.tagName.toLowerCase()).toBe('code')
+    expect(codeNode.textContent).toBe(textContent)
+  })
+})
+
+// TODO: Link with embedded markdown (with and without title)
+// TODO: Reference resolver - Simple link
+// TODO: Reference resolver - Link with embedded markdown
+// TODO: Auto links
 describe('Link', () => {
   it('should render correctly', () => {
     const href = 'https://github.com/'
@@ -200,26 +230,14 @@ describe('Link', () => {
   })
 })
 
-describe('Inline code', () => {
-  it('should render correctly', () => {
-    const textContent = 'run()'
-    const markdown = `You can just call \`${textContent}\` to start.`
-    const dom = new JSDOM(
-      `<!DOCTYPE html>
-      <div id="root">
-        ${markdownToMarkup(markdown)}
-      </div>`
-    )
-    const root = dom.window.document.getElementById('root')
-    expect(root.children.length).toBe(1)
-    const paragraphNode = root.children[0]
-    expect(paragraphNode.children.length).toBe(1)
-    const codeNode = root.children[0].children[0]
-    expect(codeNode.tagName.toLowerCase()).toBe('code')
-    expect(codeNode.textContent).toBe(textContent)
-  })
-})
+// TODO: Ordered list of simple paragraphs
+// TODO: Ordered list of paragraphs with embedded markdown
+// TODO: Unordered list of simple paragraphs
+// TODO: Unordered list of paragraphs with embedded markdown
+// TODO: Lists in lists, lists in lists in lists
 
+// TODO: Paragraph with embedded markdown
+// TODO: Danger, warning, notice boxes
 describe('Paragraph', () => {
   it('should render correctly', () => {
     const textContent = 'Some text'
@@ -250,19 +268,3 @@ describe('Thematic break', () => {
     expect(root.children[1].tagName.toLowerCase()).toBe('hr')
   })
 })
-
-// Blockquote with embedded markdown
-// Code block (syntax highlighting, copying to clipboard)
-// Image with and without title
-// Link with embedded markdown (with and without title)
-// Paragraph with embedded markdown
-// Danger, warning, notice boxes
-// Ordered list of simple paragraphs
-// Ordered list of paragraphs with embedded markdown
-// Unordered list of simple paragraphs
-// Unordered list of paragraphs with embedded markdown
-// Lists in lists, lists in lists in lists
-// Auto links
-// Reference resolver - Simple link
-// Reference resolver - Link with embedded markdown
-// Reference resolver - Images with and without title
